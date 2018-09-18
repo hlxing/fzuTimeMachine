86721cfab4e0949cee183c031683e49c3403755f
hlx <603773962@qq.com>
2018-09-18 20:45:44 +0800

    Initial commit

---
+++ src/main/java/com/west2/fzumemoryball/FzuMemoryBallApplication.java
@@ -0,0 +1,12 @@
+package com.west2.fzumemoryball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FzuMemoryBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FzuMemoryBallApplication.class, args);
+	}
+}
