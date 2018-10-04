a76e436120c7702f8891cf15c0c0a8ffb6e384d9
hlx <603773962@qq.com>
2018-10-04 10:43:24 +0800

    :memo:环境准备

--- README.md
+++ README.md
@@ -18,10 +18,18 @@
 
 规范文档参见[BACKEND.md](https://github.com/hlxing/fzuTimeMachine/blob/master/BACKEND.md)，任何未遵循该规范的代码将拒绝合并。Pull Request前请仔细阅读该文档，如有问题可提ISSUE或私聊我
 
+### 环境准备
+
+JDK1.8+Intellij IDEA+Mysql+Redis+Natapp(可选)
+
+#### 1.安装lombok插件(魔法糖工具),减少冗余代码,并进行相关配置
+
+#### 2.创建数据库timeMachine,导入[sql文件](https://github.com/hlxing/fzuTimeMachine/blob/master/timemachine.sql),增加用户timeMachine,密码123456,并分配其相应权限
+
+#### 3.安装并启动redis
+
+#### 4.安装并启动natapp(可选,小程序授权回调及七牛云oos上传回调专用),并修改application.yml
+
 ### 接口文档
 
 接口文档参见[INTERFACE.md](https://github.com/hlxing/fzuTimeMachine/blob/master/INTERFACE.md)，包括接口概要及其具体说明
-
-### 环境准备
-
-JDK1.8+Intellij IDEA+Mysql+Redis
---
+++ timemachine.sql
@@ -0,0 +1,68 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地
+ Source Server Type    : MySQL
+ Source Server Version : 50149
+ Source Host           : localhost:3306
+ Source Schema         : timemachine
+
+ Target Server Type    : MySQL
+ Target Server Version : 50149
+ File Encoding         : 65001
+
+ Date: 04/10/2018 10:12:30
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for time
+-- ----------------------------
+DROP TABLE IF EXISTS `time`;
+CREATE TABLE `time`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NULL DEFAULT NULL,
+  `title` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `imgUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `content` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `longitude` double(255, 0) NULL DEFAULT NULL,
+  `latitude` double(255, 0) NULL DEFAULT NULL,
+  `createTime` bigint(20) NULL DEFAULT NULL,
+  `updateTime` bigint(20) NULL DEFAULT NULL,
+  `label` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `praiseNum` int(11) NULL DEFAULT NULL,
+  `checkStatus` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of time
+-- ----------------------------
+INSERT INTO `time` VALUES (13, 1, 'a nice day', 'cloud.huanglexing.com/17d906da2a0442d482db49a9646bd368', 'a b c d e', 57, 113, 1538579661161, 1538579661161, 'happy', 0, 0);
+
+-- ----------------------------
+-- Table structure for wechat_user
+-- ----------------------------
+DROP TABLE IF EXISTS `wechat_user`;
+CREATE TABLE `wechat_user`  (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `openId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `nickName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `gender` int(11) NULL DEFAULT NULL,
+  `avatarUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `province` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `city` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `language` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createTime` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`userId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of wechat_user
+-- ----------------------------
+INSERT INTO `wechat_user` VALUES (1, 'oi4T64i99AgPnFrPl4SEz9oaOKFo', 'Hlx', 1, 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKPt3hefZ5CMYbCiag7icR4vUKffpian3Pbtovm54TE5hO2FfY7RTLCDcrSr8EU60hfibnw05LHEXICeA/132', 'China', 'Fujian', 'Xiamen', 'zh_CN', 1538474959);
+
+SET FOREIGN_KEY_CHECKS = 1;
